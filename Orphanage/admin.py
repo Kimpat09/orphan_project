0c8a0b0ebe356df0c1c7a0f8ffa50895059b1e58
Kimpat <142657241+Kimpat09@users.noreply.github.com>
2023-12-01 15:01:00 +0800

    Update admin.py

--- Orphanage/admin.py
+++ Orphanage/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from.models import Children
+from.models import Admin
+from.models import Adoption
+from.models import StatusForChildren
+
+admin.site.register(Children)
+admin.site.register(Admin)
+admin.site.register(Adoption)
+admin.site.register(StatusForChildren)
