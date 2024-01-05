404fe853b76979c8638c69a5a0c7392d0e16191d
Kimpat <142657241+Kimpat09@users.noreply.github.com>
2024-01-05 14:55:17 +0800

    Update models.py

--- Orphanage/models.py
+++ Orphanage/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class Children(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    sur_name = models.CharField(max_length=30)
     age       = models.IntegerField()
     religon   = models.CharField(max_length=30)
-    location  = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.sur_name}"
 
 class Admin(models.Model):
     children    = models.ForeignKey(Children, on_delete=models.CASCADE)
@@ -16,17 +18,21 @@ class Admin(models.Model):
 class Adoption(models.Model):
     children        = models.ForeignKey(Children, on_delete=models.CASCADE)
     first_name      = models.CharField(max_length=30)
-    date_of_birth   = models.IntegerField()
-    address         = models.CharField(max_length=30)
-    phone_number    = models.IntegerField()
+    sur_name        = models.CharField(max_length=30)
+    date_of_birth   = models.DateField()
+    address         = models.TextField()
+    phone_number    = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f"{self.first_name} {self.sur_name}"
 
 
 class StatusForChildren(models.Model):
     children        = models.ForeignKey(Children, on_delete=models.CASCADE)
     adoption        = models.OneToOneField(Adoption, on_delete=models.CASCADE)
     STATUS_CHOICE  = (
-        ('A', 'Accept'),
+        ('A', 'Approved'),
         ('D', 'Denied'),
-        ('P ', 'Pending')
+        ('P', 'Pending')
     ) 
     status  = models.CharField(max_length=30    , choices = STATUS_CHOICE )
