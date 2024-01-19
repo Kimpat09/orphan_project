bb0db83451344c96cc420e50e8d61300837e9f1b
Kimpat <142657241+Kimpat09@users.noreply.github.com>
2024-01-19 16:13:04 +0800

    Update views.py

--- Orphanage/views.py
+++ Orphanage/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Children, StatusForChildren
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .forms import UserSignUpForm
+
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
@@ -11,7 +15,42 @@ def Gallery(request):
     return render(request, 'pages/Gallery.html')
 
 def Login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+            if user.is_superuser:
+                return redirect('Childrenlist')
+
+            return redirect('index')
+        
     return render(request, 'pages/Login.html')
+
+def registration (request):
+    form = UserSignUpForm()
+    if request.method == "POST":
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password2')
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            email = form.cleaned_data.get('email')
+
+            user = User.objects.create_user(
+                username=username, first_name=first_name,
+                last_name=last_name, password=password, email=email)
+            
+            login(request, user)
+            return redirect('index')
+    return render(request, 'pages/registration.html')
+
+def sign_out(request):
+    logout(request)
+    return redirect('Login')
 
 def Dashboard(request):
     return render(request, 'pages/Dashboard.html')
